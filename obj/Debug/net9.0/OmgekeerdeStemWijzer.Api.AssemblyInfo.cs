15de8dafa966888ac237233045eff35c64abab95
M-Sahin <murat.sa25@gmail.com>
2025-10-26 14:04:07 +0100

    feat: Enhance ChatController with error handling for embedding and vector search failures

--- obj/Debug/net9.0/OmgekeerdeStemWijzer.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/OmgekeerdeStemWijzer.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmgekeerdeStemWijzer.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b7efd8f3909aeca4020912c07a6d35dc464006")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4d7612a8f161fc17415e5ab6223278ec27fa38")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmgekeerdeStemWijzer.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmgekeerdeStemWijzer.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
